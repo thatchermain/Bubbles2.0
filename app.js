4cde96269d6abdbe3773e6971f75824bfe32e438
thatchermain <sas.thatchermain@gmail.com>
2020-12-30 20:23:16 +0100

    Added some FUNCTIONality

--- app.js
+++ app.js
@@ -4,50 +4,53 @@ const userBtn = document.querySelector('.form__button');
 const userResetBtn = document.querySelector('.form__reset');
 let bubblesArr = [];
     
-
-const getNumber = (bubblesArr) => {
+function getNumber(bubblesArr) {
     userBtn.addEventListener('click', (e) => {
-        // bubblesArr = []
-        e.preventDefault()
+        e.preventDefault();
         for (let i = 0; i < userInput.value; i++) {
-            
             bubblesArr.push(i);
+            userBtn.style.display = 'none';
+            userInput.style.pointerEvents = 'none'
         }
-        console.log(bubblesArr)
-    })
+    });
 }
-
-
-
-getNumber(bubblesArr);
 
 userResetBtn.addEventListener('click', () => {
     userInput.value = '';
     bubblesArr =[];
 })
 
+function createBubble(x, y) {
+    console.log('Bubble');
+    let span = document.createElement('span');
+    span.style.top = y + 'px';
+    span.style.left = x + 'px';
+    span.style.borderRadius = (Math.random() * 10) * 5 + '%';
+    span.style.borderRadius = 50 + '%';
+    let spanHeight = span.style.height = Math.random() * 100 + 'px';
+    span.style.width = spanHeight;
+    let color = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+    let borderRadius = Math.random() * 5 + 'px';
+    span.style.border = `${borderRadius} solid ${color}`;
+    span.classList.add('active');
+    container.appendChild(span);
+    setTimeout(() => {
+        container.removeChild(span);
+    }, 1500);
+}
 
-container.addEventListener('mousemove', (e) => {
-    bubblesArr.forEach((el) => {
-        let x = e.offsetX;
-        let y = e.offsetY;
+function drawBubbles() {
+    for (let i = 0; i < bubblesArr.length; i++) {
+        container.addEventListener('mousemove', (e) => {
+            let x = e.offsetX;
+            let y = e.offsetY;
+            createBubble(x, y);
+        });
+    }
+}
 
-        let span = document.createElement('span');
-        span.style.top = y + 'px';
-        span.style.left = x + 'px';
-        span.style.borderRadius = (Math.random() * 10) * 5 + '%'
-        span.style.borderRadius = 50 + '%'
-        let spanHeight = span.style.height = Math.random() * 100 + 'px';
-        span.style.width = spanHeight;
-        let color = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
-        let borderRadius = Math.random() * 5 + 'px';
-        span.style.border = `${borderRadius} solid ${color}`;
-        span.classList.add('active');
-        container.appendChild(span)
-        
-        setTimeout(() => {
-            container.removeChild(span)
-        },1500)
-
-    })
+getNumber(bubblesArr);
+container.addEventListener('click', () => {
+    drawBubbles()
 })
+ 
